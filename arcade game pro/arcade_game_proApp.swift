2333c5e50e9dbc57ef19b3e8bf53147c1628d5c1
paul moore <in2scripts@yahoo.com>
2022-11-23 10:42:19 +0000

    Initial Commit

---
+++ arcade game pro/arcade_game_proApp.swift
@@ -0,0 +1,17 @@
+//
+//  arcade_game_proApp.swift
+//  arcade game pro
+//
+//  Created by paul on 23/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct arcade_game_proApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
